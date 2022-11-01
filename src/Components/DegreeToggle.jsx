10605fea38396e34b694333c5ee53107317195f5
Iconians <claytoncripe@gmail.com>
2022-10-31 21:08:01 -0700

    built basics and have api get working

---
+++ src/Components/DegreeToggle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DegreeToggle = () => {
+  return <div>Degree Toggle</div>;
+};
+
+export default DegreeToggle;
---
+++ src/Components/ForecastComponent.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import DayCard from "./DayCard";
+import DegreeToggle from "./DegreeToggle";
+import { WEATHER_API, WEATHER_URL } from "../constants";
+
+class ForecastComponent extends React.Component {
+  state = {
+    data: [],
+    loading: false,
+    error: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    try {
+      const response = await fetch(`${WEATHER_URL}${WEATHER_API}`);
+      if (response.ok) {
+        const json = await response.json();
+        const data = json.list
+          .filter((day) => day.dt_txt.includes("00:00:00"))
+          .map((item) => ({
+            temp: item.main.temp,
+            dt: item.dt,
+            date: item.dt_txt,
+            imgId: item.weather[0].icon,
+            desc: item.weather[0].description,
+          }));
+        this.setState({
+          data,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    } catch (err) {
+      console.error("there was an error", err);
+    }
+  }
+  render() {
+    const { loading, error, data } = this.state;
+    return (
+      <div>
+        <div>Forecast Container</div>
+        <DegreeToggle />
+        {!loading ? data.map((item) => <DayCard />) : <div>Loading...</div>}
+      </div>
+    );
+  }
+}
+
+export default ForecastComponent;
